9ae263f41fd5ae1ccf0b9695bd2f05f5712979fa
Hasan <hasan.shamaa10@gmail.com>
2024-11-09 21:37:08 +0300

    commit special

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import { Sidebar } from "./Components/SeconSidebar/SeconSidebar";
+import SpecialGrid from "./components/SpecialGrid/SpecialGrid";
+import img from "./components/SpecialGrid/question.svg";
 
 function App() {
+  const rows = [
+    [img, "S-211", "UX Design", "UX Design", "UX Design", "UX Design", "S-211"],
+    [img, "S-211", "UX Design", "UX Design", "UX Design", "UX Design", "S-211"],
+  ];
   return (
-    <>
-      <Sidebar/>
-    </>
+    <div className="">
+      <Sidebar />
+      {/* <SpecialGrid rows={rows} /> */}
+    </div>
   );
 }
 
